197f148a0bc13bc66575f5a422c283d9c22a9efe
ivan stol <ivistol@gmail.com>
2025-01-15 19:02:24 +0100

    header y ffooter

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <main className="p-4">
+      <h2 className="text-2xl">Bienvenido a mi Portfolio</h2>
+      <p>Aquí puedes encontrar mis proyectos y habilidades.</p>
+    </main>
+  );
+};
+
+export default Home;
